166fd9ce7d09fda70eb332e354b21a1deea72539
PetterV <petter.vilberg@gmail.com>
2020-02-29 15:56:16 +0100

    Added camera tracking

---
+++ Assets/Scripts/Ship/CameraTracking.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    public GameObject playerShip;
+    
+    public float speed = 2.0f;
+    float offsetX;
+    float offsetZ;
+    
+    void Start(){
+        offsetX = playerShip.transform.position.x + this.transform.position.x;
+        offsetZ = playerShip.transform.position.z + this.transform.position.z;
+    }
+    void Update () {
+        float interpolation = speed * Time.deltaTime;
+        
+        Vector3 position = this.transform.position;
+        position.x = Mathf.Lerp(this.transform.position.x, (playerShip.transform.position.x + offsetX), interpolation);
+        position.z = Mathf.Lerp(this.transform.position.z, (playerShip.transform.position.z + offsetZ), interpolation);
+        
+        this.transform.position = position;
+    }
+}
