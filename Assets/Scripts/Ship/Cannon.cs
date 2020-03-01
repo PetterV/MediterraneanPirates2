2a914de88c40583a574ee5b3f03f1eaf79f2cfd4
PetterV <petter.vilberg@gmail.com>
2020-03-02 00:29:58 +0100

    Saved a script that was changed, but... what?

--- Assets/Scripts/Ship/Cannon.cs
+++ Assets/Scripts/Ship/Cannon.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
-    float cannonCooldownTime = 0.7f;
-    [SerializeField]
-    float cannonCooldown;
-    public float currentCannonCooldown;
-    void Update() {
-        cannonCooldown = cannonCooldown - Time.deltaTime;
-        currentCannonCooldown = cannonCooldown / cannonCooldownTime;
-    }
     public void Fire(GameObject ball, float shotStrength){
-        if (cannonCooldown <= 0){
             GameObject cannonBall = Instantiate(ball, transform.position, Quaternion.Euler(0f, 90f, 0f));
             Vector3 shotForce = gameObject.transform.forward * shotStrength;
             cannonBall.GetComponent<Rigidbody>().AddForce(shotForce);
-            cannonCooldown = cannonCooldownTime;
         }
     }
 }
